1b6011278a080b160a4ec75ce9173648094f8cad
Jo Park <39757235+joeunparkdev@users.noreply.github.com>
2023-10-23 15:06:24 +0900

    movie search

--- Documents/sdev/movie-search-app/js/main.js
+++ Documents/sdev/movie-search-app/js/main.js
@@ -13,42 +13,33 @@ const options = {
       Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMjJmNDQ3MDA3N2RiZjYyNmQzNzdlY2VlYWZhMmNjYiIsInN1YiI6IjY1MzA5ZThiN2ViNWYyMDBhZWMwNGUwZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.kKTUishqIajYhK8uExzcVHotagXfiN0AH6vcVxJj-yg'
     }
   };
-  
-// Corrected URL for top-rated movies
+let data = ''; //to put movie data
 const topRatedMoviesUrl = 'https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1';
-
-// Call the function to fetch and display top-rated movies when the page loads
 fetchAndDisplayMovies(topRatedMoviesUrl);
 
-// Function to fetch and display movies based on a URL
-async function fetchAndDisplayMovies(url) {
+async function fetchAndDisplayMovies(url) { 
     try {
       const response = await fetch(url, options);
       if (!response.ok) {
         throw new Error(`API 요청이 실패했습니다. 응답 코드: ${response.status}`);
       }
-      const data = await response.json();
-      displayMovies(data.results); // Display the top 20 movies
+      data = await response.json();// save movie data into data
+      displayMovies(data.results); // 최고 20 영화 표시
     } catch (error) {
       console.error('영화를 가져오는 중 오류가 발생했습니다:', error);
     }
   }
 
 // 영화 검색 및 데이터 가져오기
-searchButton.addEventListener('click', async () => {
-    try {
-        const searchTerm = searchInput.value;
-        const apiUrl = `${apiEndpoint}?api_key=${apiKey}&query=${searchTerm}`;
-
-        const response = await fetch(apiUrl);
-        if (!response.ok) {
-            throw new Error(`API 요청이 실패했습니다. 응답 코드: ${response.status}`);
-        }
-        const data = await response.json();
-        displayMovies(data.results);
-    } catch (error) {
-        console.error('영화를 가져오는 중 오류가 발생했습니다:', error);
-    }
+searchButton.addEventListener('click', () => {
+  try {
+      const searchTerm = searchInput.value; 
+      const filteredMovies = data.results.filter(movie => movie.title.toLowerCase().includes(searchTerm));//case insensitive
+      console.log(filteredMovies);
+      displayMovies(filteredMovies);
+  } catch (error) {
+      console.error('영화를 가져오는 중 오류가 발생했습니다:', error);
+  }
 });
 
 // 이벤트 핸들러에서 발생한 오류를 캐치하여 처리
@@ -56,17 +47,14 @@ window.addEventListener('error', (event) => {
     console.error('오류가 발생했습니다:', event.error);
 });
 
-//별표(5개 최대) 표시
+//별표 (최대 5개) 표시
 function displayStars(rating) {
-  const starCharacter = '\u2B50'; // Unicode escape sequence for the star emoji (⭐)
+  const starCharacter = '\u2B50'; // 별 이모티콘 유니코드 (⭐)
 
-  let starsHTML = '';
-  for (let i = 0; i < Math.round(rating/2); i++) {
-    starsHTML += starCharacter;
-  }
-  console.log(starsHTML);
+  const starCount = Math.round(rating / 2);
+  const stars = new Array(5).fill(starCharacter).fill(' ', starCount, 5).join('');
 
-  return starsHTML;
+  return stars;
 }
 
 //카드 클릭 시에는 클릭한 영화 id 를 나타내는 alert 창 표시
@@ -84,7 +72,7 @@ function displayMovies(movies) {
             movieCard.classList.add('movie-card');
             movieCard.addEventListener('click', () => {
               console.log('Card clicked');
-              displayMovieId(movie.id); // Pass the movie's ID to the function
+              displayMovieId(movie.id); 
           });
 
             const image = document.createElement('img');
@@ -97,7 +85,7 @@ function displayMovies(movies) {
 
             const rating = document.createElement('p');
             const stars = displayStars(movie.vote_average); 
-            rating.textContent = `평점: ${stars}`;
+            rating.textContent = `평점: ${stars} ${movie.vote_average}`;
 
             const releaseDate = document.createElement('p');
             releaseDate.textContent = `개봉일: ${movie.release_date}`;
@@ -105,14 +93,11 @@ function displayMovies(movies) {
             const overview = document.createElement('p');
             overview.textContent = `개요: ${movie.overview}`;
 
-            // Append elements to the movieCard
             movieCard.appendChild(image);
             movieCard.appendChild(title);
             movieCard.appendChild(rating);
             movieCard.appendChild(releaseDate);
             movieCard.appendChild(overview);
-
-            // Append the movieCard to the movieList
             movieList.appendChild(movieCard);
         }
     });
